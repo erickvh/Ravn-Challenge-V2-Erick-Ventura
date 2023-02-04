7ff39b2c09ec8ac6f1144e322b8c4bf1c5a9b0f0
Erick Ventura <erick94111@gmail.com>
2023-02-04 01:30:56 -0600

    created guard to protect manager role

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -4,16 +4,53 @@ import passport from 'passport';
 import { ProductController } from '../controllers/product.controller';
 import { productSchema, searchProductSchema, uploadImageSchema } from '../schemas/product/product.schema';
 import { paramIdInt } from '../middlewares/paramIdInt';
+import { validateAdmin } from '../middlewares/guards/admin.guard';
 
 export function productRoutes(): Router {
     const router = Router();
     router.get('/', validate({ query: searchProductSchema }), ProductController.index);
-    router.post('/create', validate({ body: productSchema }), ProductController.create);
-    router.put('/:id', validate({ body: productSchema }), paramIdInt, ProductController.update);
-    router.get('/:id', validate({ body: uploadImageSchema }), paramIdInt, ProductController.show);
-    router.delete('/:id', paramIdInt, ProductController.delete);
-    router.put('/:id/disable', paramIdInt, ProductController.disable);
-    router.put('/:id/image', paramIdInt, ProductController.uploadImage);
+
+    router.post(
+        '/create',
+        passport.authenticate('jwt', { session: false }),
+        validateAdmin,
+        validate({ body: productSchema }),
+        ProductController.create,
+    );
+
+    router.put(
+        '/:id',
+        passport.authenticate('jwt', { session: false }),
+        validateAdmin,
+        validate({ body: productSchema }),
+        paramIdInt,
+        ProductController.update,
+    );
+    router.get('/:id', paramIdInt, ProductController.show);
+
+    router.delete(
+        '/:id',
+        passport.authenticate('jwt', { session: false }),
+        validateAdmin,
+        paramIdInt,
+        ProductController.delete,
+    );
+    router.put(
+        '/:id/disable',
+        passport.authenticate('jwt', { session: false }),
+        validateAdmin,
+        paramIdInt,
+        ProductController.disable,
+    );
+
+    router.put(
+        '/:id/image',
+        passport.authenticate('jwt', { session: false }),
+        validateAdmin,
+        validate({ body: uploadImageSchema }),
+        paramIdInt,
+        ProductController.uploadImage,
+    );
 
     return router;
 }
